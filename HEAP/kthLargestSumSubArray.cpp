b930e21c6b7c3844250a00193349682e0155ca1c
Patelbhavya30 <patelbhavya964@gmail.com>
2024-12-14 12:18:38 +0530

    kth largestsum subarray with space complexity O(k)

--- HEAP/kthLargestSumSubArray.cpp
+++ HEAP/kthLargestSumSubArray.cpp
@@ -4,6 +4,27 @@
 #include <vector>
 #include <queue>
 using namespace std;
+int getKthLargest(vector<int> &arr, int k)
+{
+	//	Write your code here.
+    // Time complexity: O(n^2logk) space complexity: O(k)
+	priority_queue <int, vector<int>, greater<int> > pq;
+	for(int i=0;i<arr.size();i++){
+		int sum=0;
+          for (int j = i; j < arr.size(); j++) {
+			  sum+=arr[j];
+              if(pq.size()<k){
+                  pq.push(sum);
+              }else{
+                  if(sum>pq.top()){
+                pq.pop();
+                pq.push(sum);
+            }
+              }
+          }
+        }
+	return pq.top();
+}
 int kthLargest(vector<int> &arr, int k) {
         // code here
         priority_queue <int, vector<int>, greater<int> > pq;
