3333623134b3c88036dd22f18803859600fda1fa
Patelbhavya30 <patelbhavya964@gmail.com>
2024-12-26 22:05:29 +0530

    more optimal n queen with map to check isSafe

--- BACKTRACKING/NQueens.cpp
+++ BACKTRACKING/NQueens.cpp
@@ -11,43 +11,78 @@ vector<int> addSolution(vector<vector<int>> board){
 	}
 	return ans;
 }
- bool isSafe(int row,int col,vector<vector<int>> board,int n){
-	 for(int i=0;i<col;i++){
-		 if(board[row][i]==1)
-		 	return false;
-	 }
-	 for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
-          if(board[i][j]==1)
-		 	return false;
-	 }
-	 for(int i=row+1,j=col-1;i<n&&j>=0;i++,j--){
-          if(board[i][j]==1)
-		 	return false;
-	 }
-	 return true;
+bool isSafe(int row,int col,int n,unordered_map<int,bool> &rowCheck,unordered_map<int,bool> &upperDiagonal,unordered_map<int,bool> &lowerDiagonal){
+	 
+	 return (!rowCheck[row] && !upperDiagonal[(n-1)+(col-row)] && !lowerDiagonal[(row+col)] );
  }
-void solve(vector<vector<int>> &board,vector<vector<int>> &ans,int col,int n){
+void solve(vector<vector<int>> &board,vector<vector<int>> &ans,int col,int n,unordered_map<int,bool> &rowCheck,unordered_map<int,bool> &upperDiagonal,unordered_map<int,bool> &lowerDiagonal){
 	if(col==n){
       ans.push_back(addSolution(board));
 	  return;
 	}
 	for(int row=0;row<n;row++){
-		if(isSafe(row,col,board,n)){
+		if(isSafe(row,col,n,rowCheck,upperDiagonal,lowerDiagonal)){
 			board[row][col]=1;
-			solve(board,ans,col+1,n);
+			rowCheck[row]=true;
+			upperDiagonal[((n-1)+(col-row))]=true;
+			lowerDiagonal[(row+col)]=true;
+			solve(board,ans,col+1,n,rowCheck,upperDiagonal,lowerDiagonal);
 			board[row][col]=0;
+			rowCheck[row]=false;
+			upperDiagonal[((n-1)+(col-row))]=false;
+			lowerDiagonal[(row+col)]=false;
 		}
 	}
 }
 
-// Time complexity: O(n^n+1)
 vector<vector<int>> nQueens(int n)
 {
 	// Write your code here
 	vector<vector<int>> board(n, vector<int>(n, 0));
 	vector<vector<int>> ans;
-	solve(board,ans,0,n);
-	return ans;
+	unordered_map<int,bool> rowCheck;
+	unordered_map<int,bool> upperDiagonal;
+	unordered_map<int,bool> lowerDiagonal;
+	solve(board,ans,0,n,rowCheck,upperDiagonal,lowerDiagonal);
+	return ans;	
+}
+//  bool isSafe(int row,int col,vector<vector<int>> board,int n){
+// 	 for(int i=0;i<col;i++){
+// 		 if(board[row][i]==1)
+// 		 	return false;
+// 	 }
+// 	 for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
+//           if(board[i][j]==1)
+// 		 	return false;
+// 	 }
+// 	 for(int i=row+1,j=col-1;i<n&&j>=0;i++,j--){
+//           if(board[i][j]==1)
+// 		 	return false;
+// 	 }
+// 	 return true;
+//  }
+// void solve(vector<vector<int>> &board,vector<vector<int>> &ans,int col,int n){
+// 	if(col==n){
+//       ans.push_back(addSolution(board));
+// 	  return;
+// 	}
+// 	for(int row=0;row<n;row++){
+// 		if(isSafe(row,col,board,n)){
+// 			board[row][col]=1;
+// 			solve(board,ans,col+1,n);
+// 			board[row][col]=0;
+// 		}
+// 	}
+// }
+
+// // Time complexity: O(n!)
+// vector<vector<int>> nQueens(int n)
+// {
+// 	// Write your code here
+// 	vector<vector<int>> board(n, vector<int>(n, 0));
+// 	vector<vector<int>> ans;
+// 	solve(board,ans,0,n);
+// 	return ans;
 
 	
-}
+// }
