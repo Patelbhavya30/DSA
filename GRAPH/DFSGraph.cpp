272223690c0a85697f2ba6cf399b22c711691e08
Patelbhavya30 <patelbhavya964@gmail.com>
2024-12-31 09:15:11 +0530

    all test case passed dfs graph

--- GRAPH/DFSGraph.cpp
+++ GRAPH/DFSGraph.cpp
@@ -2,15 +2,8 @@
 // https://www.naukri.com/code360/problems/dfs-traversal_630462?leftPanelTab=0&utm_source=youtube&utm_medium=affiliate&utm_campaign=Lovebabbar&leftPanelTabValue=SUBMISSION
 #include<bits/stdc++.h>
 using namespace std;
-void getAdjList(vector<vector<int>> edges,vector<vector<int>> &adjList){
 
-    for(auto i:edges){
-        adjList[i[0]].push_back(i[1]);
-        adjList[i[1]].push_back(i[0]);
-    }
-
-}
-void solve(int node,unordered_map<int,bool> &visited,vector<vector<int>> adjList,vector<int> &connectedNodes){
+void solve(int node,unordered_map<int,bool> &visited,vector<vector<int>> &adjList,vector<int> &connectedNodes){
      if(visited[node]){
          return;
      }
@@ -25,15 +18,15 @@ vector<vector<int>> depthFirstSearch(int V, int E, vector<vector<int>> &edges)
 {
     // Write your code here
     vector<vector<int>> adjList(V);
-    getAdjList(edges,adjList);
+    for(int i=0;i<edges.size();i++){
+        adjList[edges[i][0]].push_back(edges[i][1]);
+        adjList[edges[i][1]].push_back(edges[i][0]);
+    }
     unordered_map<int,bool> visited;
     vector<vector<int>> dfs;
-    vector<int> connectedNodes;
-    solve(0,visited,adjList,connectedNodes);
-    dfs.push_back(connectedNodes);
     for(int i=0;i<V;i++){
         if(!visited[i]){
-            connectedNodes.clear();
+            vector<int> connectedNodes;
             solve(i,visited,adjList,connectedNodes);
             dfs.push_back(connectedNodes);
         }
