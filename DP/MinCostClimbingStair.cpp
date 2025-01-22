8ab52c1dc2eecd2f5f1482b3aeea29c2b0ed7b9c
Patelbhavya30 <patelbhavya964@gmail.com>
2025-01-22 17:52:38 +0530

    min cost climbing optimized

--- DP/MinCostClimbingStair.cpp
+++ DP/MinCostClimbingStair.cpp
@@ -4,25 +4,53 @@
 using namespace std;
 class Solution {
 public:
-int minCost(int n,vector<int>& cost,int last,unordered_map<int,int> &dp){
-    if(n<=last){
-        return cost[last];
-    }
-    auto it = dp.find(n);
-if (it != dp.end()) {
-    return dp[n];
-} 
-    if(dp[n]){
+
+//Top down (recursion + momization)
+int minCost(int n,vector<int>& cost,vector<int> &dp){
+    if(n==0)
+        return cost[0];
+    if(n==1)
+        return cost[1];
+    
+    if(dp[n]!=-1){
         return dp[n];
     }
-    int mini=min(minCost(n-1,cost,last,dp),minCost(n-2,cost,last,dp));
+    int mini=min(minCost(n-1,cost,dp),minCost(n-2,cost,dp));
     dp[n]=mini+cost[n];
     return dp[n];
 }
+
+// Tabulation
+int bottomUp(int n,vector<int>& cost,vector<int> &dp){
+    
+    dp[0]=cost[0];
+    dp[1]=cost[1];
+    for(int i=2;i<n;i++){
+        dp[i]=cost[i]+min(dp[i-1],dp[i-2]);
+    }
+    return min(dp[n-1],dp[n-2]);
+}
+
+// Space Optimized
+int spaceOp(int n,vector<int>& cost){
+    
+    int prev1=cost[0];
+    int prev2=cost[1];
+    for(int i=2;i<n;i++){
+        int temp=cost[i]+min(prev1,prev2);
+        prev1=prev2;
+        prev2=temp;
+    }
+    return min(prev1,prev2);
+}
+
+
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
-        unordered_map<int,int> dp0;
-        unordered_map<int,int> dp1;
-        return min(min(minCost(n-1,cost,0,dp0),minCost(n-2,cost,0,dp0)),min(minCost(n-1,cost,1,dp1),minCost (n-2,cost,1,dp1)));
+        vector<int> dp(n+1,-1);
+        // dp[n]=0;
+        // return min(minCost(n-1,cost,dp),minCost(n-2,cost,dp));
+        // return bottomUp(n,cost,dp);
+        return spaceOp(n,cost);
     }
 };
