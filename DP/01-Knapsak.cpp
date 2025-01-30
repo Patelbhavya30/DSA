d63a57e6120e307d9c8cc67e1a01c2d7aefda208
Patelbhavya30 <patelbhavya964@gmail.com>
2025-01-30 22:21:51 +0530

    optmized knapsack

--- DP/01-Knapsak.cpp
+++ DP/01-Knapsak.cpp
@@ -60,6 +60,56 @@ int tab(vector<int>& weight,int n, vector<int>& value, int capacity){
 		return dp[n-1][capacity];
 
 }
+int spaceOp(vector<int>& weight,int n, vector<int>& value, int capacity){
+		vector<int> prev(capacity+1,0);
+		vector<int> curr(capacity+1,0);
+		
+		for(int w=weight[0];w<=capacity;w++){
+				if(weight[0]<=capacity)
+					prev[w]=value[0];
+				else
+					prev[w]=0;		
+		}
+
+		for(int index=1;index<n;index++){
+			for(int w=0;w<=capacity;w++){
+				int include=0;
+				if(weight[index]<=w)
+					include=value[index]+prev[w-weight[index]];
+		
+				int exclude=prev[w];
+
+				curr[w]= max(include,exclude);
+			}
+			prev=curr;
+		}
+		return prev[capacity];
+
+}
+int moreSpaceOp(vector<int>& weight,int n, vector<int>& value, int capacity){
+		vector<int> curr(capacity+1,0);
+		
+		for(int w=weight[0];w<=capacity;w++){
+				if(weight[0]<=capacity)
+					curr[w]=value[0];
+				else
+					curr[w]=0;		
+		}
+
+		for(int index=1;index<n;index++){
+			for(int w=capacity;w>=0;w--){
+				int include=0;
+				if(weight[index]<=w)
+					include=value[index]+curr[w-weight[index]];
+		
+				int exclude=curr[w];
+
+				curr[w]= max(include,exclude);
+			}
+		}
+		return curr[capacity];
+
+}
 int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight) 
 {
 	// Write your code here
@@ -68,6 +118,10 @@ int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight)
 	// vector<vector<int>> dp(n,vector<int>(maxWeight+1,-1));
 	// return recMem(weight,value,n-1,maxWeight,dp);
 
-	return tab(weight,n,value,maxWeight);
+	// return tab(weight,n,value,maxWeight);
+
+	// return spaceOp(weight,n,value,maxWeight);
+
+	return moreSpaceOp(weight,n,value,maxWeight);
 
 }
