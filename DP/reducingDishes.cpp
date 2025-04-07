9797278022f61f6077814be5ee0173f902925533
Patelbhavya30 <patelbhavya964@gmail.com>
2025-04-07 23:16:27 +0530

    reducing dishes one loop solution done

--- DP/reducingDishes.cpp
+++ DP/reducingDishes.cpp
@@ -77,6 +77,23 @@ class Solution {
             // return rec(satisfaction,n,0,1,dp);
     
             // return tab(satisfaction);
-            return spaceOp(satisfaction);
+            // return spaceOp(satisfaction);
+    
+            sort(satisfaction.begin(),satisfaction.end());
+            int n = satisfaction.size();
+            
+            int Cumulative_sum = 0; // From back
+            
+            int ans = 0;
+            int cur = 0;
+            for(int i = n-1;i>=0;i--)
+            {
+                cur += Cumulative_sum + satisfaction[i];
+                Cumulative_sum += satisfaction[i];
+                
+                ans = max(ans,cur);
+            }
+            
+            return ans;
         }
     };
