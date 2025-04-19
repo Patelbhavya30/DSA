064deaa64b8095a43eb6c2f00241e5fe58ed7045
Patelbhavya30 <patelbhavya964@gmail.com>
2025-04-19 17:29:09 +0530

    longest increasing sebsequence space optimal solution

--- DP/longestIncreasingSubsequence.cpp
+++ DP/longestIncreasingSubsequence.cpp
@@ -85,14 +85,36 @@ class Solution {
           
         
     }
+    int solveOptimal(int n,vector<int> a){
+        if(n==0)
+            return 0;
+        
+        vector<int> ans;
+        ans.push_back(a[0]);
+        
+        for(int i=1;i<n;i++){
+            if(a[i]>ans.back()){
+                ans.push_back(a[i]);
+            }
+            else{
+                int index= lower_bound(ans.begin(),ans.end(),a[i])- ans.begin();
+                ans[index]=a[i];
+            }
+            
+        }
+        
+        return ans.size();
+    }
       int lis(vector<int>& arr) {
           // code here
-        //   int n=arr.size();
+          int n=arr.size();
         //   vector<vector<int>> dp(n,vector<int>(n+1,-1));
         //   return rec(arr,n,0,-1,dp);
         
         // return tab(arr);
-        return space(arr);
+        // return space(arr);
+        
+        return solveOptimal(n,arr);
           
           
       }
