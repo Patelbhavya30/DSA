1eaa3ad8f6bed3be14c8b0e223b2ca174c8e997f
Patelbhavya30 <patelbhavya964@gmail.com>
2025-05-14 17:50:29 +0530

    pizza with 3n slices completed.

--- DP/pizzaWith3nSlices.cpp
+++ DP/pizzaWith3nSlices.cpp
@@ -23,17 +23,81 @@ class Solution {
     
         return dp[n][index]=max(take,nottake);
     }
+    int tab(vector<int>& slices){
+        int k= slices.size();
+            
+        vector<vector<int>> dp1(k+2,vector<int>(k+2,0));
+        vector<vector<int>> dp2(k+2,vector<int>(k+2,0));
+    
+        for(int index=k-2;index>=0;index--){
+            for(int n=1;n<=k/3;n++){
+                int take = slices[index] + dp1[index+2][n-1];
+                int nottake = 0 +  dp1[index+1][n];
+                dp1[index][n]=max(take,nottake);
+            }     
+        }
+    
+        for(int index=k-1;index>=1;index--){
+            for(int n=1;n<=k/3;n++){
+                int take = slices[index] + dp2[index+2][n-1];
+                int nottake = 0 +  dp2[index+1][n];
+                dp2[index][n]=max(take,nottake);
+            }     
+        }
+    
+        return max(dp1[0][k/3],dp2[1][k/3]);
+    
+    }
+    int spaceOp(vector<int>& slices){
+        int k= slices.size();
+            
+        vector<int> prev1(k+2,0);
+        vector<int> curr1(k+2,0);
+        vector<int> next1(k+2,0);
+    
+        vector<int> prev2(k+2,0);
+        vector<int> curr2(k+2,0);
+        vector<int> next2(k+2,0);
+        
+        
+    
+    
+        for(int index=k-2;index>=0;index--){
+            for(int n=1;n<=k/3;n++){
+                int take = slices[index] + next1[n-1];
+                int nottake = 0 +  curr1[n];
+                prev1[n]=max(take,nottake);
+            }
+            next1=curr1;
+            curr1=prev1;     
+        }
+    
+        for(int index=k-1;index>=1;index--){
+            for(int n=1;n<=k/3;n++){
+                int take = slices[index] + next2[n-1];
+                int nottake = 0 +  curr2[n];
+                prev2[n]=max(take,nottake);
+            }
+            next2=curr2;
+            curr2=prev2;     
+        }
+    
+        return max(curr1[k/3],curr2[k/3]);
+    
+    }
         int maxSizeSlices(vector<int>& slices) {
             int k= slices.size();
             // int case1=rec(0,k-2,slices,k/3);
             // int case2=rec(1,k-1,slices,k/3);
             
-            vector<vector<int>> dp1(k,vector<int>(k,-1));
-            vector<vector<int>> dp2(k,vector<int>(k,-1));
-            int case1=recMem(0,k-2,slices,k/3,dp1);
-            int case2=recMem(1,k-1,slices,k/3,dp2);
+            // vector<vector<int>> dp1(k,vector<int>(k,-1));
+            // vector<vector<int>> dp2(k,vector<int>(k,-1));
+            // int case1=recMem(0,k-2,slices,k/3,dp1);
+            // int case2=recMem(1,k-1,slices,k/3,dp2);
     
+            // return max(case1,case2);
     
-            return max(case1,case2);
+            // return tab(slices);
+            return spaceOp(slices);
         }
     };
